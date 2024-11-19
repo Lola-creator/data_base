93690596f718692e9fda880598606b15d0983b86
Lola-creator <facunaper@gmail.com>
2024-11-18 23:19:37 -0500

    solo falta implementar constructor por copia

--- cAccesorio.cpp
+++ cAccesorio.cpp
@@ -12,8 +12,8 @@ cAccesorio::cAccesorio() : cPrenda()
 }
 
 cAccesorio::cAccesorio(char * name, char *code, char * measure, int tam_cod, int tam_name,
-                    int tam_measure,float money, char *base_material, int tam_base, 
-					float money_base, float money_final) 
+                    int tam_measure,float money, char *base_material, int tam_base,
+					char *utility_material, int tam_utility, float money_base, float money_final)
 			: cPrenda(name, code, measure, tam_cod,tam_name, tam_measure, money)
 {
 	cPrenda::fecha.actualizarFecha();
@@ -21,19 +21,28 @@ cAccesorio::cAccesorio(char * name, char *code, char * measure, int tam_cod, int
 	
 	if(llenado_material_base(base_material, tam_base))
 	{
-		std::cout<<""<<std::endl;
+		std::cout<<"MATERIAL BASE LLENADO CON EXITO"<<std::endl;
 	}
 	else
 	{
-		std::cout<<""<<std::endl;
+		std::cout<<"MATERIAL BASE NO LLENADO CON EXITO"<<std::endl;
 	}
+	if(llenado_utilidad(utility_material, tam_utility))
+	{
+		std::cout<<"UTILIDAD LLENADO CON EXITO"<<std::endl;
+	}
+	else
+	{
+		std::cout<<"UTILIDAD LLENADO NO CON EXITO"<<std::endl;
+	}
+
 	if(llenado_precio_base(money_base))
 	{
-		std::cout<<""<<std::endl;
+		std::cout<<"PRECIO DE LA BASE LLENADO CON EXITO"<<std::endl;
 	}
 	else
 	{
-		std::cout<<""<<std::endl;
+		std::cout<<"PRECIO DE LA BASE NO LLENADO CON EXITO"<<std::endl;
 	}
 	
 	
@@ -42,7 +51,28 @@ cAccesorio::cAccesorio(char * name, char *code, char * measure, int tam_cod, int
 void cAccesorio::llenado_nullptr()
 {
 	material_base = nullptr;
+	utilidad = nullptr;
 	
+}
+
+bool cAccesorio::llenado_utilidad(char *cadena, int size)
+{
+	cPrenda::fecha.actualizarFecha();
+    if(cadena != nullptr)
+    {
+        if(utilidad != nullptr)
+        {   cPrenda::liberar(&utilidad);  }
+
+        if(llenado_size_utilidad(size,cadena))
+        {
+            cPrenda::crear_memoria(size_base, &utilidad);
+            cPrenda::copiar(cadena, utilidad, size_utilidad);
+            return true;
+        }
+        else
+        {   return false;   }
+    }
+    return false;
 }
 
 bool cAccesorio::llenado_material_base(char *cadena, int size)
@@ -50,7 +80,7 @@ bool cAccesorio::llenado_material_base(char *cadena, int size)
 	cPrenda::fecha.actualizarFecha();
     if(cadena != nullptr)
     {
-        if(unidad_relleno != nullptr)
+        if(unidad_material_base!= nullptr)
         {   cPrenda::liberar(&unidad_relleno);  }
 
         if(llenado_size_base(size,cadena))
@@ -83,6 +113,24 @@ bool cAccesorio::llenado_size_base(int size, char *cadena)
     return true;
 }
 
+bool cAccesorio::llenado_size_utilidad(int size, char *cadena)
+{
+	cPrenda::fecha.actualizarFecha();
+    if(size <= 1)//o por deafult o no lo lleno
+    {
+        size = cPrenda::contador(cadena);
+        if(size<=1) // still 0, no se crea nada
+        {
+            size_utilidad = -1;
+            std::cout<<"CADENA DE LONGITUD 0 INVIABLE"<<std::endl;
+            return false;
+        }
+    }
+
+    size_utilidad = size;
+    return true;
+}
+
 bool cAccesorio::llenado_precio_base(float numero)
 {
 	cPrenda::fecha.actualizarFecha();
@@ -112,25 +160,37 @@ bool cAccesorio::llenado_precio_final()
 		
 const char *cAccesorio::getMaterial_base()
 {
-	
+	return material_base;
+}
+
+
+const char *cAccesorio::getUtilidad()
+{
+	return utilidad;
 }
 
 int cAccesorio::getSize_base()
 {
-	
+	return size_base;
+}
+
+int cAccesorio::getSize_utilidad()
+{
+	return size_utilidad;
 }
 
 float cAccesorio::getPrecio_base()
 {
-	
+	return precio_base;
 }
 
 float cAccesorio::getPrecio_final()
 {
-	
+	return precio_final;
 }
 
 cAccesorio::~cAccesorio()
 {
 	cPrenda::liberar(&material_base);
+	cPrenda::liberar(&utilidad);
 }
--- cAccesorio.hpp
+++ cAccesorio.hpp
@@ -8,16 +8,22 @@ class cAccesorio:public cPrenda
 	public:
 		cAccesorio();
 		cAccesorio(char * name, char *code, char * measure, int tam_cod, int tam_name,
-						 int tam_measure,float money, char *base_material, int tam_base, 
-						 float money_base, float money_final);
+				   int tam_measure,float money, char *base_material, int tam_base,
+				   char *utility_material, int tam_utility, float money_base, float money_final);
 		cAccesorio();
 		bool llenado_material_base(char * base_material, int tam_base = 0);
-		bool llenado_size_base(int size, char *base_material = nullptr);
+		bool llenado_utilidad(char * base_material, int tam_base = 0);
+
+		bool llenado_size_utilidad(int size, char *base_material = nullptr);
+		bool llenado_size_utilidad(int size, char *base_material = nullptr);
+
 		bool llenado_precio_base(float money_base);
 		bool llenado_precio_final();
 		
 		const char *getMaterial_base();
+		const char *getUtilidad();
 		int getSize_base();
+		int getSize_utilidad();
 		float getPrecio_base();
 		float getPrecio_final();
 
@@ -26,7 +32,9 @@ class cAccesorio:public cPrenda
 		void llenado_nullptr();
 		
 		char *material_base;
+		char * utilidad;
 		int size_base;
+		int size_utilidad;
 		float precio_base;
 		float precio_final;
 };
